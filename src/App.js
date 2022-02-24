09beb53ab6bd1fc54797b0885485cb0e6e4fd888
JH <magatriod7@naver.com>
2022-02-24 17:31:31 +0900

    test: test git develop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 /** @format */
 
+import { Helloworld1 } from "./Helloworld1";
+
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div>helloworld1</div>
+      <Helloworld1></Helloworld1>
       <div>helloworld2</div>
     </div>
   );
--- src/Helloworld1.jsx
+++ src/Helloworld1.jsx
@@ -1,1 +1,5 @@
 /** @format */
+
+export const Helloworld1 = () => {
+  return <div className="job-wrap">Helloworld1</div>;
+};
